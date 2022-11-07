9da1082bcdf44665f286639130bff293fc00d167
CrazyPyke <103253852+CrazyPyke@users.noreply.github.com>
2022-11-07 04:10:54 +0400

    Add files via upload

---
+++ fartobi.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float radiusi;
+    float p = 3.14;
+    
+    cout << "შეიყვანეთ წრის რადიუსი: ";
+    cin >> radiusi;
+    
+    float fartobi = (radiusi * radiusi)*p;
+    cout << fartobi;
+
+    return 0;
+}
+
